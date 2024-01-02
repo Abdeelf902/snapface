d8ffc030ffae4816876668f1460fcd99ccedab24
Abderrahmane El farouah <103990456+Abdeelf902@users.noreply.github.com>
2024-01-02 16:19:37 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { FaceSnap } from './models/face-snap.model';
 export class AppComponent implements OnInit {
   faceSnaps!: FaceSnap[];
   buttonText!: string;
-  
+
 
   ngOnInit() {
 
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
             description: 'Mon meilleur ami depuis tout petit !',
             imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 23,
             location: 'Paris'
           },
           {
@@ -28,7 +28,7 @@ export class AppComponent implements OnInit {
             description: 'Un endroit magnifique pour les randonnées.',
             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 56,
             location: 'la montagne'
           },
           {
@@ -36,7 +36,7 @@ export class AppComponent implements OnInit {
             description: 'Mmmh que c\'est bon !',
             imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 78,
             location:'la mer'
           },
           {
@@ -44,7 +44,7 @@ export class AppComponent implements OnInit {
             description: 'Un coucher de soleil spectaculaire sur la plage.',
             imageUrl: 'https://cdn.bhdw.net/im/tropical-sunset-and-palm-trees-on-the-sea-beach-wallpaper-2548_w635.webp',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 6,
             location: 'Nice',
           },
           {
@@ -52,14 +52,14 @@ export class AppComponent implements OnInit {
             description: 'Les lumières de la ville la nuit sont incroyables !',
             imageUrl: 'https://s1.1zoom.me/b5050/400/423374-svetik_3840x2160.jpg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 67,
             location: 'Paris',
           },{
             title: 'Mountain Retreat',
             description: 'Échapper à la routine dans les montagnes.',
             imageUrl: 'https://www.trip-voyages.com/wp-content/uploads/2016/02/montagne1.jpeg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 89,
             location: 'Chamonix',
           }
         ];
